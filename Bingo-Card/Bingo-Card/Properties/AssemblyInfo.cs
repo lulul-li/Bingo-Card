bc6ffd7fb07089f472e4ace20b63d412c95cdea9
lulu.li29 <s987337613@gmail.com>
2017-12-10 00:02:59 +0800

    add BingoCard class and GetCard

---
+++ Bingo-Card/Bingo-Card/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bingo-Card")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bingo-Card")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84a6ad0a-65b2-4085-a590-0f963bb0fb8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bingo-Card/Bingo-Card/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Bingo_Card
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(24, BingoCard.GetCard().Length);
+        }
+    }
+
+    public class BingoCard
+    {
+        public static string [] GetCard()
+        {
+            var card = new string[24];
+            ;
+            return card;
+        }
+    }
+}
